0ebcacdc61d3a98fa909a699144e96c1a715c040
delhombre <brunodogbase@gmail.com>
2024-08-25 17:05:56 +0200

    🔥 (User): Remove UserController, UserMapper, UserLoginFormDTO, UserRegisterFormDTO, UserSessionDTO, UserAlreadyExistsException, IAuthService, IUserService, AuthService, UserService, IBaseRepository, IExpenseRepository, IProductRepository, IUserRepository, BaseRepository, ExpenseRepository, ProductRepository, and UserRepository files and related code to clean up the project and remove unused or outdated functionalities.

--- DevOpsInventoryManagement.API/Models/UserSessionDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace DevOpsInventoryManagement.Models
-{
-    public class UserSessionDTO
-    {
-        public int Id { get; set; }
-
-        public string Username { get; set; } = null!;
-    }
-}
